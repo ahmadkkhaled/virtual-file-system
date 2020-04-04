c1117d0af8a2639e4e19771ddecbcdedb95be408
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-04-04 09:00:54 +0200

    Variable system size from arguments

--- Main.java
+++ Main.java
@@ -3,7 +3,12 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        VFileSystem vfs = new VFileSystem(10);
+        int system_size = 10; // Default system size
+        if(args.length > 0){
+            system_size = Integer.parseInt(args[0]);
+        }
+
+        VFileSystem vfs = new VFileSystem(system_size);
 
         Scanner reader = new Scanner(System.in);
         String userInput = "";
@@ -109,5 +114,6 @@ public class Main {
                 }
             }
         }
+        reader.close();
     }
 }
